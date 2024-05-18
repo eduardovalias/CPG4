faf7f9e1788be837ade5aca997a3da9daedc1444
eduardovalias <edupv123@hotmail.com>
2024-05-18 01:22:24 -0300

    Smooth moviment

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public float movSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+    Vector2 velocity;
+    public float smoothTime = 0.3F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newSpeedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+        float newSpeedY = Input.GetAxisRaw("Vertical") * movSpeed;
+        
+        speedX = Mathf.SmoothDamp(speedX, newSpeedX, ref velocity.x, smoothTime);
+        speedY = Mathf.SmoothDamp(speedY, newSpeedY, ref velocity.y, smoothTime);
+        
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
