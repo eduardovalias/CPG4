ee22f7d6f3c5e4fb6e07b1f9f4bdfb4e7fb61d78
Lucas Magalhaes <lucasdsm.dev@gmail.com>
2024-05-18 17:36:17 -0300

    Update minigame of fruits falling

--- Assets/Scripts/BasketCtrl.cs
+++ Assets/Scripts/BasketCtrl.cs
@@ -7,18 +7,32 @@ public class BasketCtrl : MonoBehaviour
     
     public float movSpeed;
     Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float newSpeedX = Input.GetAxisRaw("Horizontal") * movSpeed;
-        
+
+        float input = Input.GetAxisRaw("Horizontal");
+        float newSpeedX = input * movSpeed;
+
+        if (input > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (input < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+
         rb.velocity = new Vector2(newSpeedX, 0);
     }
 }
